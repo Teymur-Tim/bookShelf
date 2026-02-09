caa776651be162b87e90dc458e50f58b7aaf84d2
Fazilov Teymur <teymurevo1@gmail.com>
2026-02-09 16:28:36 +0400

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,12 @@ public class Main {
 //        Books book1 = new Books("1984", "George ovreal", 5);
 //        Books book2 = new Books("Les Misrables", "Viktor Hugo", 5);
 //        Books book3 = new Books("Sapiens", "Yuval Noah harari", 3.7);
+          Books book4 = new Books("Little Prince", "Antuan de Sent Ekzuperi", 5);
+//
 //        bookTable.addBooks(book1);
 //        bookTable.addBooks(book2);
 //        bookTable.addBooks(book3);
+          bookTable.addBooks(book4);
 //        List<Books> allBooks = bookTable.getAllBooks();
 //        allBooks.forEach(System.out::println);
 
